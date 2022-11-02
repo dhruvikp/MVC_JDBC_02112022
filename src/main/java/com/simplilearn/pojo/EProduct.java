4981644b8b25ccd34607385865f637cad11f916d
Dhruvik <dhruviksparikh@gmail.com>
2022-11-02 22:14:46 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/pojo/EProduct.java
@@ -0,0 +1,32 @@
+package com.simplilearn.pojo;
+
+public class EProduct {
+	private Long productId;
+	private String productName;
+	private Double productPrice;
+
+	// ALT + S + R
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+}
